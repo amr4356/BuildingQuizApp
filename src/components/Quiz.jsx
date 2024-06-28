d042265bbb6d7388f7c9e619f607133f37425ff7
amr4356 <amrooo73321@gmail.com>
2024-06-28 18:35:42 +0300

    197. Working with Effect Dependencies & useCallback

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useCallback } from "react";
 import QUESTIONS from '../questions.js';
 
 import quizCompleteImg from '../assets/quiz-complete.png';
@@ -8,13 +8,14 @@ export default function Quiz(){
     const [userAnswers,setUserAnswers]=useState([]);
     const activeQuestionIndex=userAnswers.length;
     const quizIsComplete=activeQuestionIndex===QUESTIONS.length;
-
-
-    function handleSelectAnswer(selectedAnswer){
+    
+    const handleSelectAnswer=useCallback(function handleSelectAnswer(selectedAnswer){
         setUserAnswers(prevUserAnswers=>{
             return [...prevUserAnswers,selectedAnswer];
         });
-    }
+    },[]);
+
+    const handleSkipAnswer=useCallback(()=>handleSelectAnswer(null),[handleSelectAnswer]);
 
     if(quizIsComplete){
         return <div id="summary">
@@ -30,7 +31,7 @@ export default function Quiz(){
 
         <div id="quiz">
             <div id="question">
-                <QuestionTimer timeout={10000} onTimeout={()=>handleSelectAnswer(null)} />
+                <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} />
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
                     {shuffledAnswers.map(answer=>
